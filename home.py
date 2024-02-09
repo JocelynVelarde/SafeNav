fbb0753ce406c7389ccfeb41656f8f5bcfc9c8c2
JocelynVelarde <a01285780@itesm.mx>
2024-02-09 13:10:11 -0600

    added pages to sidebar, still missing content

---
+++ home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Simulation")
+st.write("Here is our super important simulation")
+
+st.sidebar.markdown("## Controls")
+st.sidebar.markdown("You can **change** the values to change the *chart*.")
---
+++ pages/about.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome to the Home Page")
+st.write("This is the main content of the home page.")
