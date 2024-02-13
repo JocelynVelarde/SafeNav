290472e2cc674c01959a9c950143ef8d0723c35d
Diego <dperezrossi@gmail.com>
2024-02-12 21:32:52 -0600

    map plot fix

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ st.write("Do you often feel unsafe when going home? Want to know the most optima
 st.write("Then, SafeNav is the answer for you! Just easily type or speak your desired initial point and final destination point, and we will provide you with the safest route to get there.")
 
 st.subheader(":blue[It is this easy to view your route on an interactive map with understandable instructions.]")
-
+"""
 df_map = pd.read_csv("assets/files/polygons.csv")
 df_map['geometry'] = df_map['geometry'].apply(wkt.loads)
 gdf = gpd.GeoDataFrame(df_map, crs='epsg:6362')
@@ -20,7 +20,7 @@ container = st.container(border=True)
 
 container.map(gdf)
 
-"""
+
 
 #failed iterations of maps
 gdf = gpd.read_file('assets/files/polygons.csv')
@@ -36,7 +36,15 @@ df = pd.DataFrame(
 container = st.container(border=True)
 container.map(df)
 """
+mexico_city_coords = [19.4326, -99.1332]
 
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + mexico_city_coords,
+    columns=['lat', 'lon'])
+
+
+container = st.container(border=True)
+container.map(df)
 st.divider()
 
 st.page_link("pages/get_started.py", label="Click this button to Get Started", icon="🚀")
